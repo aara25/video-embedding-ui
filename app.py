7cee67a4b2bd6e04f21c6ec7b09df400ef1d3d31
aara25 <aarathisekar2505@gmail.com>
2026-03-01 22:43:03 +0530

    Changed the credentials

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ from docx import Document
 from google.oauth2 import service_account
 
 
-credentials = service_account.Credentials.from_service_account_file(
-   st.secrets["gcp_service_account"]
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
 )
 
 PROJECT_ID = "video-embedding-488510"
